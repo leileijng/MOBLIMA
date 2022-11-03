145fdce633514551b93aa47688ff2769f8f3e11d
DZhang <dxzhang49@gmail.com>
2022-11-03 23:35:31 +0800

    Update List Top 5 Movies by Ratings or Revenue
    - Print only title and relative attributs for top five movie sorting by
      revenue or ratings instead of all infomation
    - Update test cases for above functions

--- MOBILIMA/src/model/MovieDB.java
+++ MOBILIMA/src/model/MovieDB.java
@@ -1,6 +1,5 @@
 package model;
 
-
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
@@ -15,14 +14,20 @@ public class MovieDB {
     public void addMovie(Movie movie) { movieList.add(movie); }
     public void printTop5MovieByRevenue() {
         movieList.sort(Comparator.comparingDouble(Movie::getTotalRevenue).reversed());
+        System.out.println("Top 5 Movie By Revenue:");
         for (int i = 0; i < Math.min(5, movieList.size()); ++i) {
-            movieList.get(i).printInfo();
+            Movie movie = movieList.get(i);
+            System.out.printf("%d\tTitle:%s\tRevenue:%.2f\n",
+                    i, movie.getMovieTitle(), movie.getTotalRevenue());
         }
     }
     public void printTop5MovieByRatings() {
         movieList.sort(Comparator.comparingDouble(Movie::getOverallRating).reversed());
+        System.out.println("Top 5 Movie By Ratings: ");
         for (int i = 0; i < Math.min(5, movieList.size()); ++i) {
-            movieList.get(i).printInfo();
+            Movie movie = movieList.get(i);
+            System.out.printf("%d\tTitle:%s\tRevenue:%.1f\n",
+                    i, movie.getMovieTitle(), movie.getOverallRating());
         }
     }
 
@@ -40,8 +45,10 @@ public class MovieDB {
         for (int i = 0; i < 6; ++i) {
             movies[i] = new Movie("jvpr"+ i);
             movies[i].addReview(new Review(123, movies[i], i%5+1, "comment "+i));
+            movies[i].addRevenue(100 * i);
             movieDB.addMovie(movies[i]);
         }
         movieDB.printTop5MovieByRatings();
+        movieDB.printTop5MovieByRevenue();
     }
 }
